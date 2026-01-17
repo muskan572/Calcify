71c80931e10a3fafc853d0ada21b51cc3080f1c3
muskan572 <muskanchhimpa2022@gmail.com>
2026-01-17 21:40:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const output = document.getElementById("output");
+  const buttons = document.querySelectorAll("button");
+
+  let expression = "";
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const value = button.innerText;
+
+      if (value === "=") {
+        try {
+          expression = eval(expression);
+          output.value = expression;
+        } catch (error) {
+          output.value = "Error";
+          expression = "";
+        }
+        return;
+      }
+      if (value === "AC") {
+        expression = "";
+        output.value = "";
+        return;
+      }
+
+      expression += value;
+      output.value = expression;
+    });
+  });
+});
